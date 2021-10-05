adc660c8b9ddc1cf2dbea25dca9204d08a6928a4
I <wx257osn2@yahoo.co.jp>
2021-10-06 06:53:33 +0900

    make it carotene_core

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,127 @@
 #include<symboli/prelude.hpp>
 #include<optional>
 #include<cstddef>
-#include<nlohmann/json.hpp>
-#include<fstream>
 #include<memory>
 #include<iostream>
+#include<mutex>
+#include<condition_variable>
+#include<thread>
+#include"symboli/carotene/core_version.hpp"
 
 static std::optional<symboli::prelude> prelude;
 
-struct config_t{
-	struct{
-		bool request;
-		bool response;
-	}save;
-	std::filesystem::path export_directory;
-}static config = {
-	.save = {
-		.request = false,
-		.response = false
-	},
-	.export_directory = ""
-};
-static inline void from_json(const nlohmann::json& j, config_t& conf){
-	auto config_opt_read = [](const nlohmann::json& j, const nlohmann::json::object_t::key_type& key, auto& value){
-		prelude->config_read<true>("Symboli Carotene :: config_read", j, key, value);
-	};
-	if(j.contains("save") && j["save"].is_object()){
-		config_opt_read(j["save"], "request", conf.save.request);
-		config_opt_read(j["save"], "response", conf.save.response);
-	}
-	if(j.contains("export_directory") && j["export_directory"].is_string()){
-		std::string str;
-		j["export_directory"].get_to(str);
-		conf.export_directory = str;
-	}
+extern "C"{
+
+__declspec(dllexport) unsigned int major_version(){
+	return SYMBOLI_CAROTENE_CORE_EXPECTED_VERSION_MAJOR;
 }
 
-will::expected<void, std::error_code> write_file(std::filesystem::path path, const char* buffer, std::size_t len){
-	::FILE* fp;
-	auto ret = ::fopen_s(&fp, path.string().c_str(), "wb");
-	if(ret)
-		return will::make_unexpected(std::error_code{ret, std::generic_category()});
-	std::unique_ptr<::FILE, decltype(&::fclose)> _{fp, &::fclose};
-	::fwrite(buffer, 1, len, fp);
-	return {};
+__declspec(dllexport) unsigned int minor_version(){
+	return SYMBOLI_CAROTENE_CORE_EXPECTED_VERSION_MINOR;
 }
+
+__declspec(dllexport) unsigned int patch_version(){
+	return SYMBOLI_CAROTENE_CORE_EXPECTED_VERSION_PATCH;
+}
+
+__declspec(dllexport) void* get_prelude(){
+	return static_cast<void*>(&*prelude);
+}
+
+}
+
+class task_t{
+	struct input{
+		const char* ptr;
+		int size;
+		enum class message_type{
+			request,
+			response
+		}type;
+	}input_data = {nullptr, 0};
+	std::mutex mtx;
+	std::condition_variable cv_ready_to_copy;
+	std::condition_variable cv_finish_copy;
+	bool met_demise = false;
+	std::vector<std::function<void(const std::vector<std::byte>&)>> request_funcs;
+	std::vector<std::function<void(const std::vector<std::byte>&)>> response_funcs;
+	template<input::message_type Type>
+	void copy(const char* ptr, int size){
+		std::unique_lock lock{mtx};
+		if constexpr(Type == input::message_type::request){
+			if(request_funcs.empty())
+				return;
+		}
+		else if constexpr(Type == input::message_type::response){
+			if(response_funcs.empty())
+				return;
+		}
+		input_data.ptr = ptr;
+		input_data.size = size;
+		input_data.type = Type;
+		cv_ready_to_copy.notify_one();
+		cv_finish_copy.wait(lock, [&]{return input_data.ptr == nullptr || met_demise;});
+	}
+public:
+	task_t() = default;
+	void demise(){
+		std::scoped_lock lock{mtx};
+		met_demise = true;
+		cv_ready_to_copy.notify_all();
+		cv_finish_copy.notify_all();
+	}
+	void request(const char* ptr, int size){
+		this->copy<input::message_type::request>(ptr, size);
+	}
+	void response(const char* ptr, int size){
+		this->copy<input::message_type::response>(ptr, size);
+	}
+	void add_request_func(std::function<void(const std::vector<std::byte>&)> f){
+		std::scoped_lock lock{mtx};
+		request_funcs.emplace_back(std::move(f));
+	}
+	void add_response_func(std::function<void(const std::vector<std::byte>&)> f){
+		std::scoped_lock lock{mtx};
+		response_funcs.emplace_back(std::move(f));
+	}
+	void operator()(){
+		while(true){
+			std::vector<std::byte> data;
+			input::message_type type;
+			{
+				std::unique_lock lock{mtx};
+				cv_ready_to_copy.wait(lock, [&]{return input_data.ptr != nullptr || met_demise;});
+				if(met_demise)
+					break;
+				data.assign(reinterpret_cast<const std::byte*>(input_data.ptr), reinterpret_cast<const std::byte*>(input_data.ptr+input_data.size));
+				type = input_data.type;
+				input_data = {};
+				cv_finish_copy.notify_one();
+			}
+			switch(type){
+			case input::message_type::request:
+				for(auto&& f : request_funcs)try{
+					f(data);
+				}catch(const std::exception& e){
+					prelude->diagnostic("Carotene Core :: request", e.what());
+				}
+				break;
+			case input::message_type::response:
+				for(auto&& f : response_funcs)try{
+					f(data);
+				}catch(const std::exception& e){
+					prelude->diagnostic("Carotene Core :: response", e.what());
+				}
+			}
+		}
+	}
+}static task = {};
+static std::thread task_thread;
 
 struct lz4_decompress_safe_ext : symboli::hook_func<int(char*, char*, int, int), lz4_decompress_safe_ext>{
 	static int func(char* src, char* dst, int compressed_size, int dst_capacity){
 		const auto ret = orig(src, dst, compressed_size, dst_capacity);
-		if(config.save.response){
-			const auto current_time = std::to_string(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count());
-			write_file(config.export_directory/(current_time+"R.msgpack"), dst, static_cast<std::size_t>(ret));
-		}
+		task.response(dst, ret);
 		return ret;
 	}
 };
@@ -60,13 +129,18 @@ struct lz4_decompress_safe_ext : symboli::hook_func<int(char*, char*, int, int),
 struct lz4_compress_default_ext : symboli::hook_func<int(char*, char*, int, int), lz4_compress_default_ext>{
 	static int func(char* src, char* dst, int src_size, int dst_capacity){
 		const auto ret = orig(src, dst, src_size, dst_capacity);
-		if(config.save.request){
-			const auto current_time = std::to_string(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count());
-			write_file(config.export_directory/(current_time+"Q.msgpack"), src, static_cast<std::size_t>(src_size));
-		}
+		task.request(src, src_size);
 		return ret;
 	}
 };
+
+__declspec(dllexport) void add_request_func(std::function<void(const std::vector<std::byte>&)> f){
+	task.add_request_func(std::move(f));
+}
+
+__declspec(dllexport) void add_response_func(std::function<void(const std::vector<std::byte>&)> f){
+	task.add_response_func(std::move(f));
+}
 
 static inline BOOL process_attach(HINSTANCE hinst){
 	const std::filesystem::path plugin_path{will::get_module_file_name(hinst).value()};
@@ -79,12 +153,16 @@ static inline BOOL process_attach(HINSTANCE hinst){
 		const auto LZ4_compress_default_ext =+ libnative.get_proc_address<int(char*, char*, int, int)>("LZ4_compress_default_ext");
 		prelude->hook<lz4_compress_default_ext>(LZ4_compress_default_ext);
 	});
+
+	task_thread = std::thread{std::ref(task)};
 	return TRUE;
 }
 
 static inline BOOL process_detach(){
 	if(prelude)
 		prelude.reset();
+	task.demise();
+	task_thread.join();
 	return TRUE;
 }
 
@@ -98,6 +176,6 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID)try{
 		return TRUE;
 	}
 }catch(std::exception& e){
-	::MessageBoxA(nullptr, e.what(), "Symboli Renderer exception", MB_OK|MB_ICONERROR|MB_SETFOREGROUND);
+	::MessageBoxA(nullptr, e.what(), "Carotene Core exception", MB_OK|MB_ICONERROR|MB_SETFOREGROUND);
 	return FALSE;
 }
